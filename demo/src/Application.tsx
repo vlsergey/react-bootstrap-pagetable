a1663e3fbbdc63aacb05206f743241045108e8e4
Sergey Vladimirov <vlsergey@gmail.com>
2021-05-14 16:22:14 +0300

    Better navigation for demo site

--- demo/src/Application.tsx
+++ demo/src/Application.tsx
@@ -4,19 +4,29 @@ import ContolledDemo from './ContolledDemo';
 import FiltersExample from './FiltersExample';
 import ItemModelExample from './ItemModelExample';
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import RoutedExample from './RoutedExample';
 import UncontrolledDemo from './UncontrolledDemo';
 
 export default class Application extends PureComponent<unknown> {
   render (): ReactNode {
     return <HashRouter>
-      <Nav className="mr-auto">
-        <Nav.Link as={Link} key="contolled" to="/controlled">Contolled PageTable Demo</Nav.Link>
-        <Nav.Link as={Link} key="uncontrolled" to="/uncontrolled">Uncontrolled PageTable Demo</Nav.Link>
-        <Nav.Link as={Link} key="itemModelExample" to="/itemModelExample"><code>ItemModel</code> example</Nav.Link>
-        <Nav.Link as={Link} key="filtersExample" to="/filtersExample">Filters example</Nav.Link>
-        <Nav.Link as={Link} key="routedExample" to="/routedExample">PageTable with react-router</Nav.Link>
-      </Nav>
+      <Navbar expand="lg">
+        <Navbar.Brand href="#">React Bootstrap Pagetable Demo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar" />
+        <Navbar.Collapse id="navbar">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} key="contolled" to="/controlled">Contolled PageTable Demo</Nav.Link>
+            <Nav.Link as={Link} key="uncontrolled" to="/uncontrolled">Uncontrolled PageTable Demo</Nav.Link>
+            <Nav.Link as={Link} key="itemModelExample" to="/itemModelExample"><code>ItemModel</code> example</Nav.Link>
+            <Nav.Link as={Link} key="filtersExample" to="/filtersExample">Filters example</Nav.Link>
+            <Nav.Link as={Link} key="routedExample" to="/routedExample">PageTable with react-router</Nav.Link>
+          </Nav>
+          <Navbar.Text>
+            <a href="https://github.com/vlsergey/react-bootstrap-pagetable">GitHub</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
       <Switch>
         <Route component={UncontrolledDemo} exact key="/uncontrolled" path="/uncontrolled" />
         <Route component={ContolledDemo} exact key="/controlled" path="/controlled" />
