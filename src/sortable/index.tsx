5e5df67980900c2fc68ec3f9033b7db234cc1909
Sergey Vladimirov <vlsergey@gmail.com>
2021-07-28 12:02:00 +0300

    Rewrite withSortable as function

--- src/sortable/index.tsx
+++ src/sortable/index.tsx
@@ -1,37 +1,34 @@
-import React, {PureComponent, ReactNode} from 'react';
 import ControlledPropsType from '../controlled/ControlledPropsType';
+import React from 'react';
 import SortableHeaderCell from './SortableHeaderCell';
 
 export type RequiredChildComponentProps<T> =
   Pick<ControlledPropsType<T>, 'columnHeaderCell' | 'fetchArgs' | 'itemModel' | 'onFetchArgsChange'>;
 
 const withSortable = <T, P extends RequiredChildComponentProps<T>>(Child: React.ComponentType<P>):
-React.ComponentType<Omit<P, 'columnHeaderCell'>> =>
-    class WithSortable extends PureComponent<Omit<P, 'columnHeaderCell'>> {
+React.ComponentType<Omit<P, 'columnHeaderCell'>> => {
+  const WithSortable = (props: Omit<P, 'columnHeaderCell'>): JSX.Element => {
+    const {fetchArgs, itemModel, onFetchArgsChange, ...etcProps} = props;
 
-      static defaultProps = Child.defaultProps;
+    const tableSortable = itemModel.fields.some(({sortable}) => sortable);
 
-      override render (): ReactNode {
-        const {fetchArgs, itemModel, onFetchArgsChange, ...etcProps} = this.props;
+    if (!tableSortable) {
+      return <Child
+        {...etcProps as P}
+        fetchArgs={fetchArgs}
+        itemModel={itemModel}
+        onFetchArgsChange={onFetchArgsChange} />;
+    }
 
-        const tableSortable = itemModel.fields.some(({sortable}) => sortable);
-
-        if (!tableSortable) {
-          return <Child
-            {...etcProps as P}
-            fetchArgs={fetchArgs}
-            itemModel={itemModel}
-            onFetchArgsChange={onFetchArgsChange} />;
-        }
-
-        return <Child
-          {...etcProps as P}
-          columnHeaderCell={SortableHeaderCell}
-          fetchArgs={fetchArgs}
-          itemModel={itemModel}
-          onFetchArgsChange={onFetchArgsChange} />;
-      }
-
-    };
+    return <Child
+      {...etcProps as P}
+      columnHeaderCell={SortableHeaderCell}
+      fetchArgs={fetchArgs}
+      itemModel={itemModel}
+      onFetchArgsChange={onFetchArgsChange} />;
+  };
+  WithSortable.defaultProps = Child.defaultProps;
+  return WithSortable;
+};
 
 export default withSortable;
