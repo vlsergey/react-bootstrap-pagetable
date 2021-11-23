9c05dfeb7e368f06edb92420d7ca3d5a260248b8
Sergey Vladimirov <vlsergey@gmail.com>
2021-11-23 12:13:00 +0300

    Provide selected items to action button renderer

--- src/actions/ActionButtonPropsType.ts
+++ src/actions/ActionButtonPropsType.ts
@@ -1,9 +1,10 @@
 import {ButtonProps}
   from 'react-bootstrap/Button';
 
-interface ActionButtonPropsType {
+interface ActionButtonPropsType<T> {
   children: ButtonProps['children'];
   disabled: ButtonProps['disabled'];
+  selectedItems: T[];
   onClick: (...etc: unknown[]) => unknown;
   size?: ButtonProps['size'];
   variant: ButtonProps['variant'];
